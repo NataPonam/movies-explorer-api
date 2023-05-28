673beebec655e447f54d83f6566c3cb992219cb5
Nata Ponamarenko <nuts08air@inbox.ru>
2023-05-28 19:08:55 +0800

    11

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const {
-  NODE_ENV, JWT_SECRET, DATABASE, PORT = 3000 || 3001,
+  NODE_ENV, JWT_SECRET, DATABASE, PORT = 3001 || 3000,
 } = process.env;
 const MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const JWT_SECRET_STRING = 'dev-secret';
